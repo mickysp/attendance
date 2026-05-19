695f8905822b99653c445fcaa41866cfe7d648fb
Siraprapa Patta <siraprapapatta@gmail.com>
2026-05-19 11:20:22 +0700

    icon tab website fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ const notoSansThai = Noto_Sans_Thai({
 });
 
 export const metadata: Metadata = {
-  title: "Attendy",
+  title: "Attendy | Attendance Management",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
